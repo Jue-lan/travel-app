9f960192ea7c2bbe1fd58f31639ad236d31eb24b
Jacob Pagan <jake2415@gmail.com>
2022-01-20 16:30:37 -0600

    made it so that we can see the list of cities

--- wayfarer/src/app/app.module.ts
+++ wayfarer/src/app/app.module.ts
@@ -7,16 +7,19 @@ import { HomepageComponent } from './header/homepage/homepage.component';
 import { PostComponent } from './info-module/post/post.component';
 import { CitiesComponent } from './cities-module/cities/cities.component';
 import { InfoComponent } from './info-module/info/info.component';
+import { CitiesModuleModule } from './cities-module/cities-module.module';
+import { HeaderModule } from './header/header.module';
+import { InfoModuleModule } from './info-module/info-module.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomepageComponent,
-    PostComponent,
-    CitiesComponent,
-    InfoComponent,
+  declarations: [AppComponent, HomepageComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    CitiesModuleModule,
+    HeaderModule,
+    InfoModuleModule,
   ],
-  imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
 })
