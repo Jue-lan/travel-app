db380d5d64278608c62ea9ead58e2708a4ec09ac
Jacob Pagan <jake2415@gmail.com>
2022-01-21 11:55:00 -0600

    Implemented a search feature, very basic right now needs refining

--- wayfarer/src/app/app-routing.module.ts
+++ wayfarer/src/app/app-routing.module.ts
@@ -2,22 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomepageComponent } from './header/homepage/homepage.component';
 
-import { InfoComponent } from './info-module/info/info.component';
-import { PostComponent } from './info-module/post/post.component';
 import { CitiesComponent } from './cities-module/cities/cities.component';
 
 const routes: Routes = [
   {
-    path: 'cities/:id',
+    path: 'cities',
     component: CitiesComponent,
-  },
-  {
-    path: 'info/:id',
-    component: InfoComponent,
-  },
-  {
-    path: 'post/:id',
-    component: PostComponent,
   },
 ];
 
