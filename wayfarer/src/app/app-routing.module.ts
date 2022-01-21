3874ad9a068764b02d8a7739718ef6bbb2cbdce4
Jacob Pagan <jake2415@gmail.com>
2022-01-21 10:54:40 -0600

    Made some initial routing changes

--- wayfarer/src/app/app-routing.module.ts
+++ wayfarer/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomepageComponent } from './header/homepage/homepage.component';
+import { PostComponent } from './info-module/post/post.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'cities',
+    component: PostComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- wayfarer/src/app/info-module/info/info.component.ts
+++ wayfarer/src/app/info-module/info/info.component.ts
@@ -3,13 +3,13 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-info',
   templateUrl: './info.component.html',
-  styleUrls: ['./info.component.css']
+  styleUrls: ['./info.component.css'],
 })
 export class InfoComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  findSomePost(value: Array<string>): Array<string> {
+    return Array<string>();
   }
-
 }
